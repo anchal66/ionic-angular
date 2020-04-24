19b483d39e1169dc2cbb8199a46de8cc3fbc8da8
Avinash Anchal <anchalavinash66@gmail.com>
2020-04-25 00:04:29 +0530

    Adding delete confirm popup

--- src/app/recipes/recipe-detail/recipe-detail.page.ts
+++ src/app/recipes/recipe-detail/recipe-detail.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { RecipesService } from '../recipes.service';
 import { Recipe } from '../recipe.model';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -13,27 +14,43 @@ export class RecipeDetailPage implements OnInit {
   loadedRecipe: Recipe;
 
   constructor(private activatdRoute: ActivatedRoute,
-     private recipeService: RecipesService,
-     private router: Router) { }
+    private recipeService: RecipesService,
+    private router: Router,
+    private alertCon: AlertController) { }
 
   ngOnInit() {
-    this.activatdRoute.paramMap.subscribe(param=>
-      {
-        if(!param.has('recipeId')){
-          //redirect
-          return;
-        }
-        const recipeId = param.get('recipeId');
-        console.log('Id: ', recipeId);
-        console.log("Loaded Recipe: ", this.recipeService.getRecipe(recipeId));
-        this.loadedRecipe = this.recipeService.getRecipe(recipeId);
-      });
+    this.activatdRoute.paramMap.subscribe(param => {
+      if (!param.has('recipeId')) {
+        //redirect
+        return;
+      }
+      const recipeId = param.get('recipeId');
+      console.log('Id: ', recipeId);
+      console.log("Loaded Recipe: ", this.recipeService.getRecipe(recipeId));
+      this.loadedRecipe = this.recipeService.getRecipe(recipeId);
+    });
   }
 
-  onDelete(){
-    this.recipeService.deleteRecipe(this.loadedRecipe.id);
-    console.log('deleted')
-    this.router.navigate(['/recipes'])
+  onDelete() {
+    this.alertCon.create({
+      header: 'Are You sure ?',
+      message: 'Do You want to delete?',
+      buttons: [{
+        text: 'Cancel',
+        role: 'cancel'
+      },
+      {
+        text: 'Delete',
+        handler:()=>{
+        this.recipeService.deleteRecipe(this.loadedRecipe.id);
+        console.log('deleted');
+        this.router.navigate(['/recipes']);
+      }
+      }]
+    }).then(alertEl=> {
+        alertEl.present();
+    })
+    
   }
 
 }
