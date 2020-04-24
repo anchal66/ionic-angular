7cf0f4b5713aafef98cb28f3f48fb2a9f7c07f14
Avinash Anchal <anchalavinash66@gmail.com>
2020-04-24 19:56:40 +0530

    Adding recipes model and view them on html

--- src/app/recipes/recipes.page.ts
+++ src/app/recipes/recipes.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Component({
   selector: 'app-recipes',
@@ -6,6 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes.page.scss'],
 })
 export class RecipesPage implements OnInit {
+
+  recipes: Recipe[]= [
+    {
+      id: 'r1',
+      title: 'Bread',
+      imageUrl: 'https://api.time.com/wp-content/uploads/2015/07/bread.jpeg?quality=85&w=1200&h=628&crop=1',
+      ingredients: ['bla', 'bla']
+    },
+    {
+      id: 'r2',
+      title: 'Laura',
+      imageUrl: 'https://thumbs.dreamstime.com/z/norwegian-fast-food-sausage-sasuage-pancake-consisting-potato-pancake-51750796.jpg',
+      ingredients: ['bla', 'bla']
+    }
+  ]
 
   constructor() { }
 
